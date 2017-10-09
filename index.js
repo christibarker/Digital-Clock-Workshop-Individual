972e412aec53abc19987d52bcb117ee32ec57cb9
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-10-09 17:14:40 -0600

    correctmerge

--- index.js
+++ index.js
@@ -8,14 +8,6 @@ function displayTime() {
 	var m = d.getMinutes();
 	var s = d.getSeconds();
 
-function displayTime() {
-	
-	var d= new Date();
-	var h= d.getHours();
-	var m= d.getMinutes();
-	var s= d.getSeconds();
-	var color = '#' + h + m + s;
-
 	if(s < 10) s = '0' + s;
 	if(m < 10) m = '0' + m;
 	if(h < 10) h = '0' + h;
@@ -32,9 +24,27 @@ document.getElementById('digital_clock').innerHTML = time;
 	else if (h < 24) {
 		document.getElementById('text').innerText = 'Afternoon';
 	}
-	
-	document.body.style.background = color;
-	document.getElementById('clock').innerText = color;	
+
 }
 
+displayTime();
 setInterval (displayTime, 1000);
+
+function hex_clock() {
+	var d = new Date();
+	var h = d.getHours();
+	var m = d.getMinutes();
+	var s = d.getSeconds();
+
+	if(s < 10) s = '0' + s;
+	if(m < 10) m = '0' + m;
+	if(h < 10) h = '0' + h;
+
+	var color = '#' + h + m + s;
+
+	document.body.style.background = color;
+	document.getElementById('clock').innerText = color;	
+
+}
+
+setInterval (hex_clock, 1000);
